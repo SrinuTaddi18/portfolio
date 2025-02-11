98bdb3a43cf54426834d9dcc067aee7bc6363c4b
SrinuTaddi18 <taddisrinu18@gmail.com>
2025-02-11 15:20:16 +0530

    Updated project with new changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import leaf from "../../Assets/Projects/wastefood.jpeg";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/swiggy.png";
 import chatify from "../../Assets/Projects/food delivery.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import suicide from "../../Assets/Projects/portfolio.png";
 import bitsOfCode from "../../Assets/Projects/task.jpeg";
 
 function Projects() {
@@ -54,29 +54,29 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
+           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="WASTE FOOD MANAGEMENT SYSTEM"
+              description="• This project helps to handle the leftover food that is collected from weddings and events. We collaborated with a non-profit organization, which will use the WFMs and distribute the excess food to those in need.Using the extra food gathered at weddings, parties, and events, the project's successful conclusion would feed the underprivileged and needy."
+              ghLink="https://github.com/SrinuTaddi18/portfolio.git"
+              // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
+           <Col md={4} className="project-card">
+             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Portfolio Website"
+              description="A personal portfolio website built using React.js to showcase projects, skills, and achievements in a visually engaging and interactive way. It features a responsive design, smooth navigation, and optimized performance for a seamless user experience. The website includes dedicated sections for project highlights, an about me page, and a contact form with social media integration. Deployed on GitHub Pages, it ensures easy accessibility and continuous updates."
+              ghLink="https://github.com/SrinuTaddi18/portfolio.git"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
@@ -86,7 +86,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col> */}
+          </Col>  */}
         </Row>
       </Container>
     </Container>
